a6097bf26774961776a2100007acf161eea22d8f
willtryagain <41161981+willtryagain@users.noreply.github.com>
2020-09-22 16:41:21 +0530

    Add files via upload

--- pipe.c
+++ pipe.c
@@ -1,42 +1,64 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#define BUFFER_SIZE 25
+#include "headers.h"
+#define SIZE 77
 #define READ_END 0
 #define WRITE_END 1
 
-int main(void) {
-	char write_msg[BUFFER_SIZE] = "aman";
-	char read_msg[BUFFER_SIZE];
-	int fd[2];
-	pid_t pid;
-	/*create the pipe*/
-	if (pipe(fd) == -1) {
-		fprintf(stderr, "Pipe failed");
-		return 1;
+int pd[2];
+void run(int argc, char **argv) {
+	
+	pipe(pd);
+
+	if (!fork()) {
+		dup2(pd[1], 1);
+		execvp(argv[0], argv);
+		perror("exec");
+		abort();
+	}
+	dup2(pd[0], 0);
+	close(pd[1]);	
+	
+
+}
+
+int main(int argc, char **argv) {
+	int i;
+	int index1=0;
+	int index2=0;
+	char args1[20][SIZE]; 
+	char args2[50][SIZE];
+	char commands[SIZE];
+	fgets(commands, SIZE, stdin);
+
+	//command 1 ; command 2;  
+	char *command = strtok(commands, "|");
+	while (command != NULL) {
+		strcpy(args1[index1++], command);
+		command = strtok(NULL, "|");
 	}
 
-	/* fork a child process */
-	pid = fork();
+	int n = strlen(args1[index1-1]);	
+	args1[index1-1][n-1] = 0;
 
-	if (pid < 0) {
-		/*error in fork*/
-		fprintf(stderr, "Fork failed");
-		return 1;
+	// ls/-a ...;
+	for (int i = 0; i < index1; ++i) {
+		index2 = 0;
+		char *word = strtok(args1[i], " ");
+		while (word != NULL) {
+			strcpy(args2[index2++], word);
+			word = strtok(NULL, " ");		
+		}
+		char **args = malloc(80 * sizeof(char *));
+		for (int i = 0; i < index2; ++i)
+			args[i] = strndup(args2[i], 100);
+		args[index2] = NULL;
+		if (i != index1-1)
+			run(index2, args);		
+		else
+			execvp(args[0], args);
+
 	}
+	
 
-	if (pid > 0) { /* parent process */
-		close(fd[READ_END]);
-		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
-		close(fd[WRITE_END]);
-	} 
-	else {
-		close(fd[WRITE_END]);
-		read(fd[READ_END], read_msg, BUFFER_SIZE);
-		printf("my name is %s\n", read_msg);
-		close(fd[READ_END]);	
-	}
-
+	
 	return 0;
 }
